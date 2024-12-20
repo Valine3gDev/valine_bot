125f61c66bcd2d0b601e75e281b3193eee1b3d0d
Lapis <45414333+Lapis256@users.noreply.github.com>
2024-12-21 06:34:30 +0900

    コンフィグの再読み込みを行うコマンドを追加

---
+++ src/features/admin.rs
@@ -0,0 +1,27 @@
+use std::sync::Arc;
+
+use poise::say_reply;
+use tokio::fs::read_to_string;
+
+use crate::{config::Config, features::PError};
+
+use super::PContext;
+
+/// コンフィグを再読み込み
+#[poise::command(slash_command, ephemeral, owners_only, dm_only)]
+pub async fn reload_config(ctx: PContext<'_>) -> Result<(), PError> {
+    let Ok(config) = read_to_string("config.toml").await else {
+        return Err("Failed to read config.toml".into());
+    };
+
+    let Ok(config) = toml::from_str::<Config>(&config) else {
+        return Err("Failed to parse config.toml".into());
+    };
+
+    let mut data = ctx.serenity_context().data.write().await;
+    data.insert::<Config>(Arc::new(config));
+
+    say_reply(ctx, "Config reloaded").await?;
+
+    Ok(())
+}
